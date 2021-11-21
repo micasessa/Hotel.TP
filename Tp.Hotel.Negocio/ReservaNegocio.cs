dbfd42b40ce9cc41afb17d7e402032a9996ba37d
micasessa <micasessa@yahoo.com>
2021-11-21 17:37:50 -0300

    Creacion clases ClienteNegocio + ReservaNegocio
    Solo las creé y puse las referencias del using.

--- Tp.Hotel.Negocio/ReservaNegocio.cs
+++ Tp.Hotel.Negocio/ReservaNegocio.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 using Tp.Hotel.Entidades;
 using Tp.Hotel.AccesoDatos;
 
+
 namespace Tp.Hotel.Negocio
 {
-    public class ClienteNegocio
+    public class ReservaNegocio
     {
+
     }
 }
