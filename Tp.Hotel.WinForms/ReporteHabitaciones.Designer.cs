5f2972cfdce20e0db87309e97fe89564f45a146e
Florencia S. Sueiro <62158341+poppin-animation@users.noreply.github.com>
2021-11-21 17:24:52 -0300

    Avance Reporte Habitaciones
    Avance Reporte Habitaciones

--- Tp.Hotel.WinForms/ReporteHabitaciones.Designer.cs
+++ Tp.Hotel.WinForms/ReporteHabitaciones.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tp.Hotel.WinForms
 {
-    partial class ReporteHabitaciones
+    partial class FrmReporteHabitaciones
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this._lblRepoSeleccioneHotel = new System.Windows.Forms.Label();
+            this._cBoxRepoSeleccioneHotel = new System.Windows.Forms.ComboBox();
+            this._lBoxHabitacionesPorHotel = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // _lblRepoSeleccioneHotel
+            // 
+            this._lblRepoSeleccioneHotel.AutoSize = true;
+            this._lblRepoSeleccioneHotel.Location = new System.Drawing.Point(59, 65);
+            this._lblRepoSeleccioneHotel.Name = "_lblRepoSeleccioneHotel";
+            this._lblRepoSeleccioneHotel.Size = new System.Drawing.Size(110, 16);
+            this._lblRepoSeleccioneHotel.TabIndex = 0;
+            this._lblRepoSeleccioneHotel.Text = "Seleccione Hotel";
+            // 
+            // _cBoxRepoSeleccioneHotel
+            // 
+            this._cBoxRepoSeleccioneHotel.FormattingEnabled = true;
+            this._cBoxRepoSeleccioneHotel.Location = new System.Drawing.Point(219, 61);
+            this._cBoxRepoSeleccioneHotel.Name = "_cBoxRepoSeleccioneHotel";
+            this._cBoxRepoSeleccioneHotel.Size = new System.Drawing.Size(528, 24);
+            this._cBoxRepoSeleccioneHotel.TabIndex = 1;
+            // 
+            // _lBoxHabitacionesPorHotel
+            // 
+            this._lBoxHabitacionesPorHotel.FormattingEnabled = true;
+            this._lBoxHabitacionesPorHotel.ItemHeight = 16;
+            this._lBoxHabitacionesPorHotel.Location = new System.Drawing.Point(60, 125);
+            this._lBoxHabitacionesPorHotel.Name = "_lBoxHabitacionesPorHotel";
+            this._lBoxHabitacionesPorHotel.Size = new System.Drawing.Size(686, 276);
+            this._lBoxHabitacionesPorHotel.TabIndex = 2;
+            // 
+            // FrmReporteHabitaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ReporteHabitaciones";
+            this.Controls.Add(this._lBoxHabitacionesPorHotel);
+            this.Controls.Add(this._cBoxRepoSeleccioneHotel);
+            this.Controls.Add(this._lblRepoSeleccioneHotel);
+            this.Name = "FrmReporteHabitaciones";
+            this.Text = "Reporte Habitaciones Por Hotel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label _lblRepoSeleccioneHotel;
+        private System.Windows.Forms.ComboBox _cBoxRepoSeleccioneHotel;
+        private System.Windows.Forms.ListBox _lBoxHabitacionesPorHotel;
     }
 }
