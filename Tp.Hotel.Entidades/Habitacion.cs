13bc36f760323d566a5713623cc1631cfc884eb5
micasessa <micasessa@yahoo.com>
2021-11-21 16:07:29 -0300

    Creacion de clases Habitacion y hotel
    Incluyen: atributos, propiedades y constructores

--- Tp.Hotel.Entidades/Habitacion.cs
+++ Tp.Hotel.Entidades/Habitacion.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,39 @@ namespace Tp.Hotel.Entidades
 {
     public class Habitacion
     {
+        //Declaracion de atributos
+        private int _idHabitacion;
+        private Hotel _idHotel;
+        private int _cantidadPlazas;
+        private string _categoria;
+        private double _precio;
+        private bool _cancelable;
+        private int _usuario;
 
+        //Creacion de propiedades
+        public int IdHabitacion { get => _idHabitacion; set => _idHabitacion = value; }
+        public Hotel IdHotel { get => _idHotel; set => _idHotel = value; }
+        public int CantidadPlazas { get => _cantidadPlazas; set => _cantidadPlazas = value; }
+        public string Categoria { get => _categoria; set => _categoria = value; }
+        public double Precio { get => _precio; set => _precio = value; }
+        public bool Cancelable { get => _cancelable; set => _cancelable = value; }
+        public int Usuario { get => _usuario; set => _usuario = value; } //estoy con duda si agregarlo o no
+
+
+        //Creacion de constructores
+        public Habitacion()
+        { }
+
+        public Habitacion(int idHabitacion, Hotel idHotel, int cantidadPlazas, string categoria, double precio, bool cancelable, int usuario)
+        {
+            idHabitacion = _idHabitacion;
+            idHotel = _idHotel;
+            cantidadPlazas = _cantidadPlazas;
+            categoria = _categoria;
+            precio = _precio;
+            cancelable = _cancelable;
+            usuario = _usuario;
+        }
 
 
     }
---
+++ Tp.Hotel.Entidades/Hotel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp.Hotel.Entidades
+{
+    public class Hotel
+    {
+        //Declaracion de atributos
+        private int _idHotel;
+        private int _estrellas;
+        private string _nombre;
+        private string _direccion;
+        private bool _amenities;
+        private int _usuario;
+
+        //Creacion de propiedades
+        public int IdHotel { get => _idHotel; set => _idHotel = value; }
+        public int Estrellas { get => _estrellas; set => _estrellas = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Direccion { get => _direccion; set => _direccion = value; }
+        public bool Amenities { get => _amenities; set => _amenities = value; }
+        public int Usuario { get => _usuario; set => _usuario = value; } //estoy con duda si agregarlo o no
+
+        //Creacion de constructores
+        public Hotel()
+        { }
+
+
+        public Hotel(int idHotel, int estrellas, string nombre, string direccion, bool amenities, int usuario)
+        {
+            idHotel = _idHotel;
+            estrellas = _estrellas;
+            nombre = _nombre;
+            direccion = _direccion;
+            amenities = _amenities;
+            usuario = _usuario;
+        }
+
+    }
+}
