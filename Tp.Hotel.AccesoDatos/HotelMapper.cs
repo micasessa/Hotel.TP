938d523e9c1ac390f9e383872771b0f60cb9c363
micasessa <micasessa@yahoo.com>
2021-11-28 21:51:30 -0300

    Desarrollo Capa AccesoDatos

--- Tp.Hotel.AccesoDatos/HotelMapper.cs
+++ Tp.Hotel.AccesoDatos/HotelMapper.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tp.Hotel.Entidades;
+using Newtonsoft.Json;
+using Tp.Hotel.Entidades.Modelos;
 
 namespace Tp.Hotel.AccesoDatos
 {
-    class HotelMapper
+    public class HotelMapper
     {
+        private List<Hotel1> _hoteles;
+
+        //Para traer la lista de HOTELES:
+        public List<Hotel1> TraerTodos()
+        {
+            string json2 = WebHelper.Get("Hotel/Hoteles/{registro}"); //FALTA COMPLETAR TEMA USUARIO/IdHOtel
+            List<Hotel1> resultado = MapList(json2);
+            _hoteles = resultado;
+            return _hoteles;
+        }
+
+        private List<Hotel1> MapList(string json)
+        {
+            List<Hotel1> lst = JsonConvert.DeserializeObject<List<Hotel1>>(json);
+            return lst;
+        }
+
+        //Para agregar Hoteles al servidor (Con un post)
+        public TransactionResult Agregar(Hotel1 hotel)
+        {
+            NameValueCollection obj = ReverseMap(hotel);
+            string json = WebHelper.Post("Hotel/Hoteles", obj);
+            TransactionResult lst = JsonConvert.DeserializeObject<TransactionResult>(json);
+            _hoteles.Add(hotel);
+            return lst;
+        }
+
+        private NameValueCollection ReverseMap(Hotel1 hotel)
+        {
+            NameValueCollection hot = new NameValueCollection();
+            hot.Add("estrellas", hotel.Estrellas.ToString());
+            hot.Add("nombre", hotel.Nombre);
+            hot.Add("direccion", hotel.Direccion);
+            hot.Add("amenities", hotel.Amenities.ToString());
+            hot.Add("usuario", hotel.Usuario.ToString());
+            return hot;
+        }
+
     }
 }
--- Tp.Hotel.AccesoDatos/ReservaMapper.cs
+++ Tp.Hotel.AccesoDatos/ReservaMapper.cs
@@ -6,12 +6,50 @@ using System.Text;
 using System.Threading.Tasks;
 using Tp.Hotel.Entidades;
 using Newtonsoft.Json;
+using Tp.Hotel.Entidades.Modelos;
 
 namespace Tp.Hotel.AccesoDatos
 {
     public class ReservaMapper
     {
+        private List<Reserva> _reserva;
 
+        //Para traer la lista de Reservas:
+        public List<Reserva> TraerTodos()
+        {
+            string json2 = WebHelper.Get("Hotel/Reservas/{registro}"); //FALTA COMPLETAR TEMA USUARIO/IdHOtel
+            List<Reserva> resultado = MapList(json2);
+            _reserva = resultado;
+            return _reserva;
+        }
+
+        private List<Reserva> MapList(string json)
+        {
+            List<Reserva> lst = JsonConvert.DeserializeObject<List<Reserva>>(json);
+            return lst;
+        }
+
+        //Para agregar Reservas al servidor (Con un post)
+        public TransactionResult Agregar(Reserva reserva)
+        {
+            NameValueCollection obj = ReverseMap(reserva);
+            string json = WebHelper.Post("Hotel/Reservas", obj);
+            TransactionResult lst = JsonConvert.DeserializeObject<TransactionResult>(json);
+            _reserva.Add(reserva);
+            return lst;
+        }
+
+        private NameValueCollection ReverseMap(Reserva reserva)
+        {
+            NameValueCollection res = new NameValueCollection();
+            res.Add("idHabitacion", reserva.IdHabitacion.ToString());
+            res.Add("idCliente", reserva.IdCliente.ToString());
+            res.Add("cantidadHuespedes", reserva.CantidadHuespedes.ToString());
+            res.Add("fechaIngreso", "2021-11-29T00:49:02.390Z");
+            res.Add("fechaEgreso", "2021-11-29T00:49:02.390Z");
+            res.Add("usuario", reserva.Usuario.ToString());
+            return res;
+        }
 
     }
 }
