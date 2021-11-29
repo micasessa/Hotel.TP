4ca5e1a17a1b8f88dc7f85f57461434fe5321575
Florencia S. Sueiro <62158341+poppin-animation@users.noreply.github.com>
2021-11-28 22:11:40 -0300

    Actualizacion Negocio
    Cliente y Habitacion Negocio

--- Tp.Hotel.Negocio/ClienteNegocio.cs
+++ Tp.Hotel.Negocio/ClienteNegocio.cs
@@ -5,10 +5,55 @@ using System.Text;
 using System.Threading.Tasks;
 using Tp.Hotel.Entidades;
 using Tp.Hotel.AccesoDatos;
+using Tp.Hotel.Entidades.Modelos;
 
 namespace Tp.Hotel.Negocio
 {
     public class ClienteNegocio
     {
+
+        private List<Cliente> _clientes;
+        private ClienteMapper _clienteMapper;   
+
+
+        public ClienteNegocio()
+        {
+            _clientes = new List<Cliente>();
+        }
+
+
+        public List<Cliente> TraerClientes()
+        {
+            try
+            {
+                return _clienteMapper.TraerTodos();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+        public TransactionResult AltaCliente(Cliente cliente)
+        {
+
+
+            TransactionResult resultado = _clienteMapper.Agregar(cliente);
+            if (resultado.IsOk == false)
+            {
+                throw new Exception("No se pudo realizar el Alta del Cliente.");
+                }
+            else
+            {
+                return resultado;
+            }
+        }
+
+
+
+
+
     }
 }
