80fc6cb06f829180f153db9ad69db8e5964d1c96
Florencia S. Sueiro <62158341+poppin-animation@users.noreply.github.com>
2021-11-29 08:43:40 -0300

    Avance Negocio
    Avance de Cliente, habitaciones, hotel y reserva negocios con llamados a mappers y reportes

--- Tp.Hotel.Negocio/HotelNegocio.cs
+++ Tp.Hotel.Negocio/HotelNegocio.cs
@@ -3,10 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tp.Hotel.AccesoDatos;
+using Tp.Hotel.Entidades;
+using Tp.Hotel.Entidades.Modelos;
 
 namespace Tp.Hotel.Negocio
 {
-    class HotelNegocio
+    public class HotelNegocio
     {
+
+        private List<Hotel1> _hoteles;
+        private HotelMapper _hotelMapper;
+
+
+        public HotelNegocio()
+        {
+            //_hoteles = new List<Hotel1>();
+            _hotelMapper = new HotelMapper();
+        }
+
+
+        public List<Hotel1> TraerHoteles() ///LISTA DE HOTELES
+        {
+            try
+            {
+                return _hotelMapper.TraerTodos();
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public TransactionResult AltaHotel(Hotel1 hotel) // ALTA DE HOTEL
+        {
+
+
+            TransactionResult resultado = _hotelMapper.Agregar(hotel);
+            if (resultado.IsOk == false)
+            {
+                throw new Exception("No se pudo realizar el Alta del Hotel.");
+            }
+            else
+            {
+                return resultado;
+            }
+        }
+
+
+
+
     }
 }
+
