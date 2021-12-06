7183558df763c16dbb817e098c67a86ba2c73554
Florencia S. Sueiro <62158341+poppin-animation@users.noreply.github.com>
2021-12-05 21:18:57 -0300

    Corrección 10 - Mejora 3
    3: "Validar nombre hotel para no cargar repetidos (NombreRepetidoException)"

---
+++ Tp.Hotel.Entidades/Exceptions/NombreRepetidoException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp.Hotel.Entidades.Exceptions
+{
+    public class NombreRepetidoException : Exception
+    {
+
+        public NombreRepetidoException(string nombre) : base("El nombre " + nombre.ToUpper() + " ya se encuentra ingresado en nuestra base de datos.")
+        {
+
+        }
+    }
+}
